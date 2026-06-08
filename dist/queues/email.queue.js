a861d5ed05231f74ef759ac7a1cd4819f3e5b368
NUR MOHAMMAD <116737444+NurMohammad56@users.noreply.github.com>
2026-06-08 08:19:07 +0600

    Delete dist directory (#1)

--- dist/queues/email.queue.js
+++
@@ -1,4 +0,0 @@
-import { Queue } from "bullmq";
-import redisConnection from "../config/redis.js";
-const emailQueue = new Queue("emailQueue", { connection: redisConnection });
-export default emailQueue;
